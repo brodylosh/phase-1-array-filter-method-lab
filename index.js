30441812a001ffec40230bd34a4351d5da677be3
Brody Losh <brodylosh@gmail.com>
2022-04-06 21:30:10 -0400

    commit tree 713d86c637cf068b008e19e87c0410b96500718b

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution here
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+
+const findMatching = (arr, string) => {
+    let result = arr.filter(element => element.toUpperCase() === string.toUpperCase());
+    return result;
+}
+
+const fuzzyMatch = (arr, string) => {
+    let result = arr.filter(element => element[0] === string[0]);
+    return result;
+}
+
+const matchName = (arr, string) => {
+    let result = arr.filter(element => element.name.toUpperCase() === string.toUpperCase());
+    return result;
+}
